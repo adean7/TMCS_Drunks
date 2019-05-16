a6226fe4b4efd58f92f84b5ab5b5c1a26d4080a0
gundelach <gundelachlennart@gmail.com>
2019-05-16 16:34:54 +0100

    adding going home decisions and active status to person class

--- person.py
+++ person.py
@@ -1,5 +1,6 @@
 """"Class of People """
 import random
+import networkx
 
 class person():
     """Class for people objects which traverse the node"""
@@ -14,18 +15,14 @@ class person():
 
         # Can generate name from list later
         self.name="Bob"
-        self.type='drunk'
-        self.home=start_node
+        self.type='random'
+        self.home='1324666940'
+        self.active=True
+        print (self.current_node)
 
         # Select a path from the starting node
         self.make_decision()
 
-
-    def random_node(self, neighbor_list):
-        """ Randomly selects the next node from the list of connected nodes """
-
-        next_node = random.choice(neighbor_list)
-        return next_node
 
 
     def make_decision(self):
@@ -34,14 +31,21 @@ class person():
         # Select the next node
         # Currently selects at random from connected nodes
         neighbors = list(self.graph.neighbors(self.current_node))
-        self.next_node = self.random_node(neighbors)
+        if self.type == 'random':
+            self.next_node = self.random_node(neighbors)
+        if self.type == 'home':
+            self.next_node=self.node_to_home()
 
         # Set speed
         # Constant for now, may change later
         self.speed = 1.0
 
         # Set the relevant positions from the nodes
-        self.new_path_positions(self.current_node, self.next_node)
+        # Check if person has reached his destinaction and is now flagged inactive
+        if self.active==True:
+            self.new_path_positions(self.current_node, self.next_node)
+        else:
+            pass
 
 
     def new_path_positions(self, start_node, final_node):
@@ -76,26 +80,28 @@ class person():
         """ Update the positions of a person
         Inputs:     dt, time step
         """
+        if self.active==True:
+            # Calculate the scalar distance moved
+            distance_moved = self.speed * dt
 
-        # Calculate the scalar distance moved
-        distance_moved = self.speed * dt
+            # Add to the total scalar distance travelled
+            self.traveled_distance += distance_moved
 
-        # Add to the total scalar distance travelled
-        self.traveled_distance += distance_moved
+            # Check if a node is reached
+            if self.check_at_node() == True:
 
-        # Check if a node is reached
-        if self.check_at_node() == True:
+                # If it is, then pick a new path
+                # Updates x,y of person to be at the new node within the function
+                self.current_node = self.next_node
+                self.make_decision()
 
-            # If it is, then pick a new path
-            # Updates x,y of person to be at the new node within the function
-            self.current_node = self.next_node
-            self.make_decision()
+            else:
 
+                # Otherwise update x,y to be along the paths
+                self.x += distance_moved * self.unit_vector[0]
+                self.y += distance_moved * self.unit_vector[1]
         else:
-
-            # Otherwise update x,y to be along the paths
-            self.x += distance_moved * self.unit_vector[0]
-            self.y += distance_moved * self.unit_vector[1]
+            pass
 
 
     def check_at_node(self):
@@ -104,5 +110,30 @@ class person():
             return True
         else:
             return False
+
+    def random_node(self, neighbor_list):
+        """ Randomly selects the next node from the list of connected nodes """
+
+        next_node = random.choice(neighbor_list)
+        return next_node
+
+    def node_to_home(self):
+        """Returns the next node on the shortest path toward home"""
+        next_node = self.node_shortest_path(self.home)
+        return next_node
+
+    def node_shortest_path(self,target):
+        """Returns the next node on the shortest path from 'current_node' to 'goal_node'"""
+        path = networkx.shortest_path(self.graph, self.current_node, target)
+        print(path)
+
+        if len(path)>1:
+            next_node=path[1]
+        else:
+            next_node=path[0]
+            self.active=False
+            print ("I am home")
+        print(next_node)
+        return next_node
 
 
