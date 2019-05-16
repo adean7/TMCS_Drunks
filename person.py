b59667d86a55ec226374998010dcd3c26af72709
gundelach <gundelachlennart@gmail.com>
2019-05-16 12:43:44 +0100

    Added placeholder functions to person class

--- person.py
+++ person.py
@@ -8,30 +8,42 @@ class people():
         # Get these from the graph object via node ID
         self.x=0
         self.y=0
-
         self.current_node=start_node #Via node ID
-
         self.name="Bob"
-
         self.next_node=self.make_decision()
-        self.calc_distance()
-        self.calc_speed()
+        self.total_distance=self.calc_distance()
+        self.traveled_distance=0
+        self.speed=self.calc_speed()
 
 
-    def make_decision(self):
+    def make_decision(self,connected_nodes):
         """Function to decide which node to travel to next based on current node"""
-        #return next node
+        #make choice from connected_nodes
+        next_node==None
+        return next node
         pass
 
     def calc_distance(selfs):
         #set distance to next node
         """Calculate distance between nodes"""
-        pass
+        distance=0
+        return distance
 
     def calc_speed(selfs):
         # define speed
         """Calculate speed to travel between selected node"""
-        self.speed=1
+        self.speed=0
+        return self.speed
+
+    def updated_distance(self):
+        """Update distance travelled"""
+
         pass
+
+
+    def check_at_node(selfs):
+        """Check if person has reached node"""
+        if self.traveled_distance>self.total_distance:
+            return True
 
 
