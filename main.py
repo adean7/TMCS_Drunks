5a5319bf5dede8f9d5e857b4992dd8a3b2874f75
gundelach <gundelachlennart@gmail.com>
2019-05-16 17:39:19 +0100

    added new functionality to person and a name generator script

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ graph = graph.CustomGraph('stuff_provided/planet_-1.275,51.745_-1.234,51.762.osm
 
 # Create a person at a starting node
 start_node = list(graph.nodes)[0]
-bob = person.person(start_node, graph)
+people_list = person.generate_people(graph,2,'home','random')
+
 
 # Have bob wander around for a bit
-for i in range(200):
-
-    bob.update_position()
-    print(i, bob.current_node, bob.next_node, bob.traveled_distance, bob.total_distance)
+for j in range(2):
+    for i in range(200):
+        people_list[j].update_position()
+        print(i, people_list[j].current_node, people_list[j].next_node, people_list[j].traveled_distance, people_list[j].total_distance)
