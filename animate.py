b22d86b7eb4094400dc842883f81cb18027a8702
Adam Dean <adam.dean@chem.ox.ac.uk>
2019-05-17 07:21:40 +0100

    Small changes to animate.py

--- animate.py
+++ animate.py
@@ -4,15 +4,13 @@ import math
 import graph
 import person
 from PIL import Image
-import random
+#import random
 
 def draw_circle(num_verts, center_x, center_y, radius):
 
-    global vertices
-
     vertices = []
 
-    # Some of these values can be cached for efficiency
+    # Some of these values (probably only angles) can be cached for efficiency
     for i in range(num_verts):
         angle_rad = 2 * i * math.pi / num_verts
         cos_theta = math.cos(angle_rad)
@@ -21,7 +19,6 @@ def draw_circle(num_verts, center_x, center_y, radius):
         vertices.append((cos_theta + sin_theta) * radius + center_y)
 
     return vertices
-
 
 
 class graphicsWindow(pyglet.window.Window):
@@ -55,10 +52,10 @@ class graphicsWindow(pyglet.window.Window):
     def convert_coordinate(self, lon, lat):
 
         lon = (lon - self.range_lon[0]) / (self.range_lon[1] - self.range_lon[0])
-        lon = lon * width
+        lon = lon * img_width
 
         lat = (lat - self.range_lat[0]) / (self.range_lat[1] - self.range_lat[0])
-        lat = lat * height
+        lat = lat * img_height
 
         return lon, lat
 
@@ -78,42 +75,38 @@ class graphicsWindow(pyglet.window.Window):
     def on_draw(self):
 
         # Clear the graphics buffer
-        pyglet.gl.glClearColor(1, 1, 1, 1)
+        #pyglet.gl.glClearColor(1, 1, 1, 1)
         pyglet.gl.glClear(pyglet.gl.GL_COLOR_BUFFER_BIT)
 
         # Draw background
         sprite.draw()
 
-#        coords = []
-#        for i in range(self.num_people):
-#            coords.append(self.x)
-#            coords.append(self.y)
-
-
+        # Set people colour and size (only use size if not using draw_circle)
         pyglet.gl.glColor3f(1, 0, 0)
         #pyglet.gl.glPointSize(10)
 
-        num_verts = 8  # Number of vertices of circle.
-        radius = 2  # Radius of circles.
+        num_verts = 8  # Number of vertices of each circle
+        radius = 2  # Radius of circles
 
         for i in range(self.num_people):
 
+            # Converts single point to list of points to form a circle
             lon, lat = self.convert_coordinate(self.x[i], self.y[i])
             coords = draw_circle(num_verts, lon, lat, radius)
 
+            # Draw circles
             coords_list = pyglet.graphics.vertex_list(num_verts, ('v2f', coords))
-            coords_list.draw(pyglet.gl.GL_POLYGON)
+            coords_list.draw(pyglet.gl.GL_POLYGON)  # Use .GL_POINTS if circles don't need to be filled
 
 
-# this is the main game engine loop
+# This is the main game engine loop
 if __name__ == '__main__':
 
     # Load background image
     image = 'oxford.png'
     im = Image.open(image)
-    width, height = im.size
-    for_sprite = pyglet.image.load_animation(image)
-    sprite = pyglet.sprite.Sprite(img=for_sprite)
+    img_width, img_height = im.size
+    sprite = pyglet.sprite.Sprite(img=pyglet.image.load_animation(image))
 
     # Load graph
     graph = graph.CustomGraph('stuff_provided/planet_-1.275,51.745_-1.234,51.762.osm.gz')
@@ -123,10 +116,11 @@ if __name__ == '__main__':
 
     # Create an instance of a window
     window = graphicsWindow(people, graph)
-    window.set_size(width, height)
+    window.set_size(img_width, img_height)
 
-    # Tell pyglet the on_draw() & update() timestep.
+    # Tell pyglet the on_draw() & update() timestep
     pyglet.clock.schedule_interval(window.update, 1 / 30.0)
 
     # Run pyglet
+    sprite.draw()
     pyglet.app.run()
